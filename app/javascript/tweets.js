7a9cd9d3e9555d2b120d921a3f79f1dfa371b8fa
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-12-14 23:29:50 +0900

    画像プレビュー実装

--- app/javascript/tweets.js
+++ app/javascript/tweets.js
@@ -30,18 +30,39 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
   });
 
   /** 思い出写真 */
-  //**document.addEventListener("DOMContentLoad",function(){
-    //**const ImageBox = document.getElementById("image-box");
-    //**document.getElementById("image-box").addEventListener("change",function(e){
-      //**const file = e.target.files[0];
-      //**const blob = window.URL.createObjectURL(file);
-      //**const imageElement = document.createElement("div");
-      //**const blobImage = document.createElement("img");
-      //**blobImage.setAttribute("src",blob);
-      //**imageElement.appendChild(blobImage);
-      //**ImageBox.appendChild(imageElement);
-    //**});
-  //**});
 
+  document.addEventListener("DOMContentLoaded",function(){
+    const ImageBox = document.getElementById("image-box");
+    
+    const createImageHTML = (blob) =>{
+      const imageElement = document.createElement("div")
+      imageElement.setAttribute("class","image-element")
+      let imageElementNum = document.querySelectorAll(".image-element").length
+      const blobImage = document.createElement("img")
+      blobImage.setAttribute("src",blob)
+      const inputHTML = document.createElement("input")
+      inputHTML.setAttribute("id", "tweet_image_${imageElementNum}")
+      inputHTML.setAttribute("name", "tweet[memory_image][]")
+      inputHTML.setAttribute("type", "file")
+
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageBox.appendChild(imageElement)
+
+      inputHTML.addEventListener("change", (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById("tweet_image").addEventListener("change", (e) =>{
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+    })
+  })
 
 }
