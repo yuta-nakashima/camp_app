e5855d3320d79bb75b0f9f4b292e779efaa563eb
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-12-22 15:17:52 +0900

    結合テストコード

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "Tweet投稿", type: :system do
+RSpec.describe "Comments", type: :system do
   before do
     @user = FactoryBot.create(:user)
-    @tweet = FactoryBot.build(:tweet)
+    @tweet = FactoryBot.create(:tweet)
+    @comment = FactoryBot.build(:comment)
   end
-  context "Tweetが投稿できるとき" do
-    it "ログインしたユーザーは新規投稿できる" do
+
+  context "Commentが投稿できるとき" do
+    it "ログインしたユーザーはコメント投稿できる" do
       # ログインする
       visit new_user_session_path
       fill_in "Email Address", with: @user.email
@@ -14,8 +16,6 @@ RSpec.describe "Tweet投稿", type: :system do
       find('input[name="commit"]').click
       expect(current_path).to eq "/"
       visit tweets_path
-      # 新規投稿ページへのリンクがあることを確認する
-      expect(page).to have_content("Tweet")
       # 投稿ページに移動する
       visit new_tweet_path
       # フォームに情報を入力する
@@ -33,15 +33,28 @@ RSpec.describe "Tweet投稿", type: :system do
       expect(current_path).to eq "/tweets.#{@user.id}"
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
       expect(page).to have_selector("img")
+      # 詳細ページに移動する
+      visit tweet_path(@tweet.id)
+      # コメント入力欄があることを確認する
+      expect(page).to have_content("思い出メモ")
+      # コメントを入力する
+      fill_in "comment_comment", with: @comment.comment
+      #コメントするとcommentのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {Comment.count}.by(1)
+      # 投稿完了後同じページに留まる
+      expect(current_path).to eq "/tweets/#{@tweet.id}"
+      # 先ほど投稿した内容のコメントが存在することを確認する
+      expect(page).to have_content(@comment_comment)
     end
   end
-  context "Tweetが投稿できないとき" do
-    it "ログインしていないと新規投稿ページに遷移できない" do
+  context "Commentが投稿できないとき" do
+    it "ログインしていないとユーザーはコメント投稿できない" do
       # ログインページに遷移する
       visit tweets_path
       # ログインページに遷移する
       expect(current_path).to eq "/users/sign_in"
     end
   end
-
 end
